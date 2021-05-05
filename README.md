057a9fc600352d19c96e5ed59a84035f007927d6
Verner Jan <jverner@evektor.cz>
2021-05-05 14:20:04 +0200

    Cleanup, better readme, update requirements

--- README.md
+++ README.md
@@ -8,3 +8,25 @@
 token="8c9d6348569bc29463209e0e21d..."
 stocks_site="https://www.notion.so/1ff359..."
 ```
+
+## Install
+
+```
+# Linux
+
+python3.8 -m venv .env
+source .env/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Windows
+
+```
+
+## Run
+
+```
+# Under virtual environment
+
+python main.py
+```
--- main.py
+++ main.py
@@ -7,28 +7,33 @@ import yfinance as yf
 
 
 def main():
-    print("This is main")
+    print("Starting...")
     # data = yf.download(tickers="AMD MNKD", period="1d", group_by="tickers")
-    # print(data)
 
     client = NotionClient(token_v2=settings.token)
 
-    degiro = client.get_collection_view(settings.stocks_site)
-    print(f"Degiro: {degiro}")
+    # Get table from Degiro page
+    tblDegiro = client.get_collection_view(settings.stocks_site)
 
     x = PrettyTable()
-    x.field_names = ["Code", "Investing URL", "Price"]
+    x.field_names = ["Code", "Investing URL", "USD"]
 
-    for row in degiro.collection.get_rows(search=""):
+    # Iterate over
+    for row in tblDegiro.collection.get_rows(search=""):
+
+        # Ignore empty rows if any
         if row.stock_name == "":
             continue
 
+        # Get current stock price from Yahoo Finances
         stock = yf.Ticker(row.stock_name)
         price = stock.info["regularMarketPrice"]
-        row.price = price
 
-        # Add to table for printing
-        x.add_row([row.stock_name, row.investing_url, row.price])
+        # Update table USD field with new price
+        row.usd = price
+
+        # Add row to table for printing
+        x.add_row([row.stock_name, row.investing_url, row.usd])
 
     print(x)
 
