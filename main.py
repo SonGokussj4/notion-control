5799ca432a75b201d696fe52f362a8fdf0bb2b13
Verner Jan <jverner@evektor.cz>
2021-05-05 17:54:48 +0200

    Better structure, writing StockShortName into the table with Date

--- main.py
+++ main.py
@@ -1,39 +1,62 @@
 #!/usr/bin/env python3
 
+import yfinance as yf
+
+from datetime import datetime, timedelta
+from notion.collection import NotionDate
 from config import settings
 from notion.client import NotionClient
 from prettytable import PrettyTable
-import yfinance as yf
 
 
 def main():
     print("Starting...")
-    # data = yf.download(tickers="AMD MNKD", period="1d", group_by="tickers")
+
+    yesterday_date = datetime.now() - timedelta(1)
 
     client = NotionClient(token_v2=settings.token)
 
     # Get table from Degiro page
     tblDegiro = client.get_collection_view(settings.stocks_site)
 
+    # Pretty table settings
     x = PrettyTable()
-    x.field_names = ["Code", "Investing URL", "USD"]
+    x.field_names = ["Code", "Investing URL", "USD", "Date"]
+    # Column Alignment
+    x.align["Code"] = "l"
+    x.align["Investing URL"] = "l"
+    x.align["USD"] = "r"
+    x.align["Date"] = "r"
+
+    # Get table data
+    table_rows = tblDegiro.collection.get_rows()
+
+    # Get all stock_tickers
+    stock_tickers = ' '.join([item.ticker for item in table_rows])
+    df = yf.download(tickers=stock_tickers, period="2d", group_by="ticker", progress=False)
+
+    tickers = yf.Tickers(stock_tickers)
 
     # Iterate over
-    for row in tblDegiro.collection.get_rows(search=""):
+    for row in table_rows:
 
         # Ignore empty rows if any
-        if row.stock_name == "":
+        if row.ticker == "":
             continue
 
-        # Get current stock price from Yahoo Finances
-        stock = yf.Ticker(row.stock_name)
-        price = stock.info["regularMarketPrice"]
+        # print(f"Checking ticker: {row.ticker}: {closing_value}")
+        ticker = tickers.tickers[row.ticker]
 
-        # Update table USD field with new price
-        row.usd = price
+        shortName = ticker.info['shortName']
+        closing_value = round(df[row.ticker]['Close'][yesterday_date.strftime("%Y-%m-%d")], 2)
+
+        # Update table USD, DATE fields with new values
+        row.usd = closing_value
+        row.name = shortName
+        row.date = NotionDate(yesterday_date)
 
         # Add row to table for printing
-        x.add_row([row.stock_name, row.investing_url, row.usd])
+        x.add_row([row.ticker, row.investing_url, row.usd, yesterday_date])
 
     print(x)
 
